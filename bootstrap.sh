1654077b8a2edb888cac64ccfdc2e83594168770
R-Pax <git@mac.hm>
2026-08-28 21:18:33 +0200

    add preferred macOS settings to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# quit on error
+set -e 
 
+# -------------- macOS --------------
 
-# Dock
+# dark mode
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+
+# dock permahide
 defaults write com.apple.dock autohide -bool true && defaults write com.apple.dock autohide-delay -float 1000 && killall Dock
-# Squared off window corners
+
+# square windows
 defaults write -g NSConvolutionOverride1 -float 0.1 && killall Finder
+
+# no auto correct 
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# finder no filename extensions/popups
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
