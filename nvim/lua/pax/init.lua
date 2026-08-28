d7d1ad8b546f544bbffb0bb529d107520f074160
R-Pax <git@mac.hm>
2026-08-28 15:16:41 +0200

    Make background transparent

--- nvim/lua/pax/init.lua
+++ nvim/lua/pax/init.lua
@@ -1,2 +1,8 @@
 require("pax.set")
 require("pax.lazy")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
