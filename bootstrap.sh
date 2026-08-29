b86750a0c2f478e28085ef0ba14f2c374db853b4
R-Pax <git@mac.hm>
2026-08-29 15:52:32 +0200

    Add echos for every step of the setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,17 +7,21 @@ set -e
 
 # dark mode
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+echo "Dark mode on"
 
 # dock permahide
 defaults write com.apple.dock autohide -bool true && defaults write com.apple.dock autohide-delay -float 1000 && killall Dock
+echo "Removed the dock"
 
 # square windows
 defaults write -g NSConvolutionOverride1 -float 0.1 && killall Finder
+echo "Windows are now squared off"
 
 # no auto correct 
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+echo "Auto correct is off"
 
 # finder no filename extensions/popups
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
