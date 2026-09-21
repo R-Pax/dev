0800799381e1c640908cadc3e73b4541acf44f9c
R-Pax <git@mac.hm>
2026-09-21 23:31:37 +0200

    Change macOS wallpaper path to black.jxl since black.png doesn't exist anymore

--- macOS.sh
+++ macOS.sh
@@ -5,7 +5,7 @@ set -e
 
 CURRENT_DIR="${CURRENT_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")/macOS" && pwd)}"
 
-WALLPAPER="$CURRENT_DIR/black.png"
+WALLPAPER="$CURRENT_DIR/black.jxl"
 
 # -------------- macOS --------------
 
